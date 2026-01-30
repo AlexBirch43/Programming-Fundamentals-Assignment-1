9ee483f924699754acbe4eac006d9dba06a4e07d
AlexBirch43 <alexbirch43@gmail.com>
2026-01-30 12:56:18 +0000

    Version 1.2 - Add in prompts for non-existent CSV file

--- Component_Tracker/file_setup.py
+++ Component_Tracker/file_setup.py
@@ -4,10 +4,11 @@ import os
 CSV_FILE = 'inventory.csv'
 
 #  Create CSV with set headers if non-existant
-# ******WOULD LIKE TO ADD IN CHECK HERE IF NOT EXISTING "WOULD YOU LIKE TO CREATE NEW CSV"
 def check_csv():
     if not os.path.exists(CSV_FILE):
+        print("No CSV file found. Creating a new inventory file for you.")
         with open(CSV_FILE, mode='w', newline='') as file:
             fieldnames = ['name', 'type', 'quantity']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
+        print("New CSV file created.")
