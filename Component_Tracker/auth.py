25687b3aa0f313fa2f0c59403ce8faf775328ff4
AlexBirch43 <alexbirch43@gmail.com>
2026-02-01 13:53:38 +0000

    Version 1.3 - Fix to allow non case sensitive username input
    Additional line required changing

--- Component_Tracker/auth.py
+++ Component_Tracker/auth.py
@@ -8,6 +8,7 @@ def validate_user(username, password):
     return username_upper in Users and Users[username_upper] == password
 
 def get_role(username):
+    username = username.upper()                                             #allows username not to be case-sensitive
     if username == "ENGINEER":
         return "ENGINEER"
     return "MANAGER"
